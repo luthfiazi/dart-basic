93f397b736e2e8ec4f84f11a1cc2a839f64015ed
luthfiazi <luthfiazizi11@gmail.com>
2021-09-29 09:38:34 +0700

    type data string lanjutan

--- string.dart
+++ string.dart
@@ -2,7 +2,7 @@ void main(){
   // var nama = 'Luthfi Azizi';
 
   // Bisa juga tanpa var
-  String nama = 'Luthfi Azizi';
+  String nama = ' Luthfi Azizi ';
   String daftarHewan = "kucing, Kuda, Ayam";
   var angka = 10;
 
@@ -25,5 +25,36 @@ void main(){
   print(nama.substring(7, 12));
   //7 -> awal huruf (masuk)
   //12 -> akhir huruf (tidak masuk)
+
+  //menampilkan panjang string
+  print(nama.length);
+
+  //menghilangkan spasi di depan dan belakang
+  print(nama.trim());
+
+  //menghilangkan spasi didepan
+  print(nama.trimRight());
+
+  //menghilangkan spasi di belakang
+  print(nama.trimLeft());
+
+  //mendapatkan nilai desimal ASCII
+  print(nama.codeUnitAt(1));
+
+  //menampilkan index karakter dalam string
+  print(nama.indexOf('i'));
+
+  //pengecekan apakah string diawali dengan string/ karakter tertentu
+  print(nama.startsWith(' luthfi'));
+  
+  //pengecekan apakah string diakhiri dengan string/ karakter tertentu
+  print(nama.endsWith('Azizi '));
+
+  //cek apakah string kosong
+  var kosong = '';
+  print(kosong.isEmpty);
+
+  //apakah string tersebut tidak kosong
+  print(kosong.isNotEmpty);
 
 }
