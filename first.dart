7e5fb5a6a38586229d61e428189a23f3595a08ff
luthfiazi <luthfiazizi11@gmail.com>
2021-09-29 08:56:55 +0700

    type data boolean

--- first.dart
+++ first.dart
@@ -1,18 +1,27 @@
 void main() {
   // print('Hello World!');
 
+  // String
   var name = 'Voyager I';
+  // Integer
   var year = 1977;
+  // Double
   var antennaDiameter = 3.7;
+  // List
   var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  // Map
   var image = {
     'tags': ['saturn'],
     'url': '//path/to/saturn.jpg'
   };
+  // boolean
+  var isLoggedIn = true;
+
   print('name: $name');
   print(year);
   print(antennaDiameter);
   print(flybyObjects[2]);
   print('planet pertama: ${flybyObjects[0]}');
   print(image);
+  print('Status login: $isLoggedIn');
 }
